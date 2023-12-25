a1c5cd2bb81f552d83a6dbbf2ab70871f5b43da3
Kevin Maik Jablonka <kjablonk@ip-172-64-55-195.us-west-2.compute.internal>
2023-12-25 23:08:44 +0000

    fix scripts

--- scripts/run_preprocessing.sh
+++ scripts/run_preprocessing.sh
@@ -1,3 +1,3 @@
 # on spark node
 export S3_BASE="s3://llched-raw/open_lm_run"
-python open_lm/datapreprocess/make_2048.py --input-files path_to_raw_data/*.jsonl --output-dir preproc_data --num-workers 32 --num-consumers 8 --upload_to_s3
+python open_lm/datapreprocess/make_2048.py --input-files /scratch/kjablonk/export/**/*.jsonl  --output-dir /scratch/kjablonk/preproc_data --num-workers 30 --num-consumers 8 --upload-to-s3
