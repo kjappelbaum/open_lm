afe61fbb1e674cbbeef6fd028edc11e71095f1f4
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-12-30 22:46:46 +0100

    use shell

--- scripts/run_preprocressing_export.py
+++ scripts/run_preprocressing_export.py
@@ -9,8 +9,8 @@ S3_BASE_BASE_DIR = "s3://llched-raw/open_lm_run_processed_data/export/"
 def run_preprocessing_export(file):
     stem = Path(file).stem
     s3_base_dir = f"{S3_BASE_BASE_DIR}/{stem}"
-    subprocess.run(["export", f"S3_BASE={s3_base_dir}"])
-    subprocess.run(["python", "open_lm/datapreprocess/make_2048.py", "--input-files", file, "--output-dir", "/scratch/kjablonk/preproc_data", "--num-workers", "30", "--num-consumers", "8", "--upload-to-s3"])
+    subprocess.run(["export", f"S3_BASE={s3_base_dir}"], shell=True)
+    subprocess.run(["python", "open_lm/datapreprocess/make_2048.py", "--input-files", file, "--output-dir", "/scratch/kjablonk/preproc_data", "--num-workers", "30", "--num-consumers", "8", "--upload-to-s3"], shell=True)
 
 
 
