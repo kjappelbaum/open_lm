26e1ba3b8d9a807530389b7ef1f84298f70e1daa
Kevin Maik Jablonka <kjablonk@spark-dy-i3en-12xlarge-3.stability-grantwestcpu.pcluster>
2024-01-02 14:55:30 +0000

    manifest

---
+++ scripts/generate_manifest_europmc.sh
@@ -0,0 +1,3 @@
+python ../open_lm/utils/make_wds_manifest.py --data-dir /scratch/kjablonk/europmc_ft_results_train/ --manifest-filename europmc_train
+python ../open_lm/utils/make_wds_manifest.py --data-dir /scratch/kjablonk/europmc_ft_results_test/ --manifest-filename europmc_test
+python ../open_lm/utils/make_wds_manifest.py --data-dir /scratch/kjablonk/europmc_ft_results_valid/ --manifest-filename europmc_valid
--- scripts/run_preprocessing_europmc.sh
+++ scripts/run_preprocessing_europmc.sh
@@ -1,8 +1,10 @@
-cd /scratch/kjablonk
+# cd /scratch/kjablonk
 
-aws s3 cp s3://llched-raw/training_v0/ft_results_train_clean.jsonl .
-aws s3 cp s3://llched-raw/training_v0/ft_results_test_clean.jsonl .
-aws s3 cp s3://llched-raw/training_v0/ft_results_valid_clean.jsonl .
+# aws s3 cp s3://llched-raw/training_v0/ft_results_train_clean.jsonl .
+# aws s3 cp s3://llched-raw/training_v0/ft_results_test_clean.jsonl .
+# aws s3 cp s3://llched-raw/training_v0/ft_results_valid_clean.jsonl .
+
+# cd /weka/home-kjablonk/open_lm
 
 python open_lm/datapreprocess/make_2048.py --input-files /scratch/kjablonk/ft_results_train_clean.jsonl --output-dir /scratch/kjablonk/preproc_data_europmc_ft_train --num-workers 30 --num-consumers 8 --upload-to-s3 --s3-base-dir s3://llched-raw/open_lm_run_processed_data/europmc_ft_results_train/
 python open_lm/datapreprocess/make_2048.py --input-files /scratch/kjablonk/ft_results_test_clean.jsonl --output-dir /scratch/kjablonk/preproc_data_europmc_ft_test --num-workers 30 --num-consumers 8 --upload-to-s3 --s3-base-dir s3://llched-raw/open_lm_run_processed_data/europmc_ft_results_test/
