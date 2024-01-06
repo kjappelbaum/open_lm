4315b8c0e0aba638270122f2f0aa9a5a39ceb986
Kevin Maik Jablonka <mail@kjablonka.com>
2024-01-06 15:50:46 +0100

    more logs

--- scripts/upload_to_s3_from_manifest.py
+++ scripts/upload_to_s3_from_manifest.py
@@ -8,6 +8,7 @@ def upload_to_s3(manifest_file):
         for line in file:
             data = json.loads(line)
             if data['num_sequences'] > 0:
+                print(f"uploading {data['manifest_path']}")
                 # Upload the path to S3
                 s3 = boto3.client('s3')
                 parts = Path(data['manifest_path']).parts
