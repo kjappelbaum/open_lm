fd6fa5b71a413368a4f83ab0112ca31188823910
Kevin Maik Jablonka <mail@kjablonka.com>
2024-01-13 19:30:40 +0100

    merge

--- open_lm/utils/make_wds_manifest.py
+++ open_lm/utils/make_wds_manifest.py
@@ -8,9 +8,10 @@ from pathlib import Path
 from cloudpathlib import CloudPath, S3Path
 from tqdm import tqdm
 from glob import glob
-from pathlib import Path 
+from pathlib import Path
 import tempfile
-import os 
+import os
+
 
 def path_or_cloudpath(s):
     if re.match(r"^\w+://", s):
@@ -41,37 +42,37 @@ def count_file(file):
     try:
         count = int(subprocess.check_output(f"tar tf {file} | wc -l", shell=True))
     except subprocess.CalledProcessError as e:
-        print(f'Failed for {file} due to {e}')
+        print(f"Failed for {file} due to {e}")
         count = 0
     return count
 
-        
+
 def count_samples(shard_path):
     if isinstance(shard_path, CloudPath):
-        print('Downloading ', shard_path)
+        print("Downloading ", shard_path)
         with tempfile.TemporaryDirectory(dir=os.getcwd()) as tempdir:
-            tmpfile = Path(tempdir) / 'archive.tar'
+            tmpfile = Path(tempdir) / "archive.tar"
             shard_path.download_to(tmpfile)
             return count_file(tmpfile)
     else:
         return count_file(shard_path)
 
+
 def worker_fn(input_data):
     shard_path, data_dir = input_data
     return {
-            "manifest_path": str(shard_path),
-            "shard": shard_path.parts[-2],
-            "num_sequences": count_samples(shard_path),
-        }
-    
+        "manifest_path": str(shard_path),
+        "shard": shard_path.parts[-2],
+        "num_sequences": count_samples(shard_path),
+    }
 
 
 def main(args):
     args = parse_args(args)
-    
+
     if isinstance(args.data_dir, S3Path):
         bucket_path = args.data_dir
-        shards = list(bucket_path.glob('**/*.tar'))
+        shards = list(bucket_path.glob("**/*.tar"))
     else:
         shards = sorted([Path(x) for x in glob(str(args.data_dir / "**" / "*.tar")) if Path(x).name.endswith(".tar")])
     input_data = [(shard, args.data_dir) for shard in shards]
@@ -85,7 +86,7 @@ def main(args):
 
     total_sequences = sum([item["num_sequences"] for item in data])
     print(f"Total sequences in {args.data_dir}: {total_sequences}")
-    manifest_path =  args.manifest_filename
+    manifest_path = args.manifest_filename
     with open(manifest_path, "w") as fp:
         for item in data:
             simdjson.dump(item, fp)
